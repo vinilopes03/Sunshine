a336f76cde41936ec4b222f87f035b616499f0bb
vinilopes03 <vinilopes03@gmail.com>
2017-07-03 19:02:32 -0300

    Projeto Sunshine commits v 2.0
    Projeto Sunshine commits v 2.0

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/example/vinic/sunshine/test/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.vinic.sunshine.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.vinic.sunshine.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from build type: debug
+  public static final String OPEN_WEATHER_MAP_API_KEY = "dbdfd8a1e1c9c20daa99ffc74044f142";
+}
